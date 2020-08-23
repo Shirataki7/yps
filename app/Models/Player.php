5b5d4c12360c861950e54535c7e1ac2ca93090b2
Shirataki7 <88naechan88@gmail.com>
2020-08-23 12:46:38 +0000

    task5/インデント修正

--- app/Models/Player.php
+++ app/Models/Player.php
@@ -13,13 +13,13 @@ class Player extends Model
     public static function getWithContoryBySimplePaginate($paginate_limit){
        try{
 	       return self::from('players as p')
-	       ->select('p.*','c.name as c_name','t.goals as t_goals')
-               ->leftjoin('c_names as c','p.id','=','c.id')
-	       ->leftjoin('total_goals as t','p.id','=','t.id')
-	       ->orderBy('t.goals','desc')
-	       ->orderBy('p.club','asc')
-	       ->orderBy('p.uniform_num','asc')
-	       ->Paginate($paginate_limit);
+	        ->select('p.*','c.name as c_name','t.goals as t_goals')
+            ->leftjoin('c_names as c','p.id','=','c.id')
+	        ->leftjoin('total_goals as t','p.id','=','t.id')
+	        ->orderBy('t.goals','desc')
+	        ->orderBy('p.club','asc')
+	        ->orderBy('p.uniform_num','asc')
+	        ->Paginate($paginate_limit);
        } catch(Throwble $e){
 	       throw $e;
        }
